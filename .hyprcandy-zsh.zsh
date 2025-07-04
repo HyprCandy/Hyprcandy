72f0e3b7c55b06b2dcef44f126abc225c0144b70
I.M KING <mirukaiano@gmail.com>
2025-07-04 16:28:10 +0300

    Update .hyprcandy-zsh.zsh

--- .hyprcandy-zsh.zsh
+++ .hyprcandy-zsh.zsh
@@ -1,10 +1,3 @@
-# Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
-# Initialization code that may require console input (password prompts, [y/n]
-# confirmations, etc.) must go above this block; everything else may go below.
-#if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
-#  source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
-#fi
-
 # Uncomment the following line if pasting URLs and other text is messed up.
 DISABLE_MAGIC_FUNCTIONS="true"
 
@@ -20,8 +13,6 @@ COMPLETION_WAITING_DOTS="true"
 # Example format: plugins=(rails git textmate ruby lighthouse)
 # Add wisely, as too many plugins slow down shell startup.
 [[ -z "${plugins[*]}" ]] && plugins=(git fzf extract)
-
-source $ZSH/oh-my-zsh.sh
 
 # User configuration
 
@@ -59,54 +50,17 @@ bindkey '^[[B' history-substring-search-down
 bindkey '^[OA' history-substring-search-up
 bindkey '^[OB' history-substring-search-down
 
-# Aliases
-alias ll="ls -alF"
-alias la="ls -A"
-alias l="ls -CF"
-alias ..="cd .."
-alias ...="cd ../.."
-alias grep="grep --color=auto"
-alias fgrep="fgrep --color=auto"
-alias egrep="egrep --color=auto"
-alias update="sudo pacman -Syu"
-alias install="sudo pacman -S"
-alias search="pacman -Ss"
-alias remove="sudo pacman -R"
-alias autoremove="sudo pacman -Rs $(pacman -Qtdq)"
-alias cls="clear"
-alias h="history"
-alias j="jobs -l"
-alias df="df -h"
-alias du="du -h"
-alias mkdir="mkdir -pv"
-alias wget="wget -c"
-
-# Git aliases
-alias g="git clone"
-alias ga="git add ."
-alias gc="git commit -m"
-alias gp="git push"
-alias gl="git pull"
-alias gs="git status"
-alias gd="git diff"
-alias gco="git checkout"
-alias gb="git branch"
-alias glog="git log --oneline --graph --decorate"
-
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 # Set personal aliases, overriding those provided by oh-my-zsh libs,
-# plugins, and themes. Aliases can be placed here, though oh-my-zsh
-# users are encouraged to define aliases within the ZSH_CUSTOM folder.
+# plugins, and themes. Aliases can be placed here.
 #alias open="xdg-open"
 alias make="make -j`nproc`"
 alias ninja="ninja -j`nproc`"
 alias n="ninja"
-alias c="clear"
 alias rmpkg="sudo pacman -Rsn"
 alias cleanch="sudo pacman -Scc"
 alias fixpacman="sudo rm /var/lib/pacman/db.lck"
-alias update="sudo pacman -Syu"
 
 # Help people new to Arch
 alias apt="man pacman"
@@ -114,21 +68,11 @@ alias apt-get="man pacman"
 alias please="sudo"
 alias tb="nc termbin.com 9999"
 
-# Cleanup orphaned packages
-alias cleanup="sudo pacman -Rsn $(pacman -Qtdq)"
-
 # Get the error messages from journalctl
 alias jctl="journalctl -p 3 -xb"
 
 # Recent installed packages
 alias rip="expac --timefmt='%Y-%m-%d %T' '%l\t%n %v' | sort | tail -200 | nl"
-
-# Fun stuff
-alias matrix="cmatrix"
-alias pipes="pipes.sh"
-
-# Zsh p10k theme
-source /usr/share/zsh-theme-powerlevel10k/powerlevel10k.zsh-theme
 
 # Fish-like syntax highlighting and autosuggestions
 source /usr/share/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
@@ -139,8 +83,5 @@ source /usr/share/zsh/plugins/zsh-history-substring-search/zsh-history-substring
 
 # pkgfile "command not found" handler
 source /usr/share/doc/pkgfile/command-not-found.zsh
-
-# To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
-[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
 
 export FZF_BASE=/usr/share/fzf
