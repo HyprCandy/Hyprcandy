be07fa2bb0374d2bdff2af98b113bf4eb3f1e574
I.M KING <mirukaiano@gmail.com>
2025-07-08 03:58:35 +0300

    latest update

--- .config/hypr/scripts/window-opacity.sh
+++ .config/hypr/scripts/window-opacity.sh
@@ -7,14 +7,10 @@ if [ -f $HOME/.config/hyprcandy/settings/gamemode-enabled ]; then
 else
     hyprctl --batch "\
         keyword animations:enabled 1;\
-        keyword decoration:shadow:enabled 0;\
+        keyword decoration:shadow:enabled 1;\
         keyword decoration:blur:enabled 1;\
         keyword decoration:active_opacity 1;\
-        keyword decoration:inactive_opacity 1;\
-        keyword decoration:rounding 10;\
-        keyword general:gaps_in 0;\
-        keyword general:gaps_out 0;\
-        keyword general:border_size 2" 
+        keyword decoration:inactive_opacity 1" 
     touch $HOME/.config/hyprcandy/settings/gamemode-enabled
     notify-send "Opacity" "Increased" -t 2000
 fi
