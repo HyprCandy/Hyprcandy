5f2ddcbba74553d7645da9d27b1c6e06df27b1b3
CANDY <mirukaiano@gmail.com>
2026-03-20 14:55:30 +0300

    Update update.sh

--- .config/hypr/scripts/update.sh
+++ .config/hypr/scripts/update.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-# UltraCandy Update Script
+# HyprCandy Update Script
 # Launches kitty in floating mode and runs the update
 
 kitty --class="floating-installer" \
       --override=initial_window_width=900 \
       --override=initial_window_height=600 \
       -e bash -c "
-rm -rf ~/ultracandyinstall
-git clone --depth 1 https://github.com/HyprCandy/ultracandyinstall.git && 
-cd ultracandyinstall && 
-bash Update_UltraCandy.sh
+rm -rf ~/hyprcandyinstall
+git clone --depth 1 https://github.com/HyprCandy/hyprcandyinstall.git && 
+cd hyprcandyinstall && 
+bash HyprCandy_Update.sh
 "
